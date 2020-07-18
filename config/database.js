5571d885407ee1f628a4c93505b1523cf34308db
josenilson <josenilson.neiva@gmail.com>
2020-07-18 16:55:21 -0300

    Atualização

--- config/database.js
+++ config/database.js
@@ -6,6 +6,10 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+const Url = require('url-parse')
+
+const DB =  new Url(Env.get('CLEARDB_DATABASE_URL'))
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -51,11 +55,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', DB.host),
+      port: Env.get('DB_PORT', DB.port),
+      user: Env.get('DB_USER', DB.username),
+      password: Env.get('DB_PASSWORD', DB.password),
+      database: Env.get('DB_DATABASE', DB.database)
     },
     debug: Env.get('DB_DEBUG', false)
   },
